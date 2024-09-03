6e56b3c6dfce024c4874e2a7a05ea3922e5e8869
Geraldo Neto <netogeraldo209@gmail.com>
2024-09-03 03:46:08 -0300

    broken sidenav

--- JeitinhoBrasileiro/src/app/header/header.component.ts
+++ JeitinhoBrasileiro/src/app/header/header.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Output, EventEmitter } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ MatIconModule],
+  imports: [HeaderComponent, 
+    FooterComponent, 
+    RouterOutlet, 
+    MatIconModule,  
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatDialogModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @Output() cartOpened = new EventEmitter<void>();
+  openCart() {
+    this.cartOpened.emit();
+  }
+  cartItems = [
+    { name: 'Acarajé', price: 50, image: 'assets/acaraje.jpg' },
+    { name: 'Pato no Tucupi', price: 60, image: 'assets/pato-no-tucupi.jpg' },
+    { name: 'Feijoada', price: 30, image: 'assets/feijoada.jpg' },
+    { name: 'Coxinha', price: 25, image: 'assets/coxinha.jpg' },
+  ]
 
 }
--- JeitinhoBrasileiro/src/app/home/home.component.ts
+++ JeitinhoBrasileiro/src/app/home/home.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, MatIconModule],
+  imports: [HeaderComponent, 
+    FooterComponent, 
+    RouterOutlet, 
+    MatIconModule,  
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatSidenavModule,
+
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('drawer') drawer!: MatSidenav;
 
+  cartItems = [
+    { name: 'Acarajé', price: 50, image: 'assets/acaraje.jpg' },
+    { name: 'Pato no Tucupi', price: 60, image: 'assets/pato-no-tucupi.jpg' },
+    { name: 'Feijoada', price: 30, image: 'assets/feijoada.jpg' },
+    { name: 'Coxinha', price: 25, image: 'assets/coxinha.jpg' },
+  ];
+
+  ngAfterViewInit() {
+  }
+
+  openCart() {
+    if (this.drawer) {
+      this.drawer.toggle(); // Abre ou fecha o drawer
+    } else {
+      console.error('Drawer não está inicializado!');
+    }
+  }
 }
