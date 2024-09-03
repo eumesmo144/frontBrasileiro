8023352ba20be760122233c402404ec2237d8da1
Geraldo Neto <netogeraldo209@gmail.com>
2024-09-03 01:26:17 -0300

    footer finalizado

--- JeitinhoBrasileiro/src/app/layouts/header/header.component.ts
+++ JeitinhoBrasileiro/src/app/layouts/header/header.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ComponentesAngularModule } from '../../componentes-angular/componentes-angular.module';
 import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ComponentesAngularModule, MatIconModule],
+  imports: [ MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
